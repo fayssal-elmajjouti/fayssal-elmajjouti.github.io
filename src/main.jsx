7b6ff4137174bb10ae2306c90f5294f7912b9322
Fayssal <fayssal.elmajjouti@gmail.com>
2025-05-26 19:06:51 +0100

    fix ghp route urls

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import ReactDOM from 'react-dom/client'  // This line might be missing
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
 
 
 createRoot(document.getElementById('root')).render(
@@ -11,8 +9,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
-// );
